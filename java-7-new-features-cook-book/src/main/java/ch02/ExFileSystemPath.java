3ae81657698db98834ac1fccdacc3835dd26303f
KwonNam Son <kwon37xi@gmail.com>
2016-01-13 23:23:13 +0900

    Path example

---
+++ java-7-new-features-cook-book/src/main/java/ch02/ExFileSystemPath.java
@@ -0,0 +1,23 @@
+package ch02;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class ExFileSystemPath {
+    public static void main(String[] args) {
+        Path path  = FileSystems.getDefault().getPath("/tmp/docs/status.txt");
+
+        System.out.println();
+        System.out.printf("toString: %s%n", path.toString());
+        System.out.printf("getFileName : %s%n", path.getFileName());
+        System.out.printf("getRoot: %s%n", path.getRoot());
+        System.out.printf("getNameCount: %d%n", path.getNameCount());
+        for (int index = 0; index < path.getNameCount(); index++) {
+            System.out.printf("getName(%d) : %s%n", index, path.getName(index));
+        }
+
+        System.out.printf("subpath(0,2) : %s%n", path.subpath(0, 2));
+        System.out.printf("getParent: %s%n", path.getParent());
+        System.out.println(path.isAbsolute());
+    }
+}
