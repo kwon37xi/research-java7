1a6ff210c2671fec25f0334ef381e2ec00f4b2a0
KwonNam Son <kwon37xi@gmail.com>
2016-01-18 23:19:05 +0900

    UsageFileMove

---
+++ java-7-new-features-cook-book/src/main/java/ch04/UsageFilesMove.java
@@ -0,0 +1,14 @@
+package ch04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class UsageFilesMove {
+    public static void main(String[] args) throws IOException {
+        Path sourceFile = Paths.get("/tmp/docs/users.txt");
+        Path destinationFile = Paths.get("/tmp/music/users.txt");
+        Files.move(sourceFile, destinationFile);
+    }
+}
