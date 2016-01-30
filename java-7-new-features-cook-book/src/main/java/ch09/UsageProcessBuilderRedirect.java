0896f9e25041010620de732c946eccf34e616652
KwonNam Son <kwon37xi@gmail.com>
2016-01-30 23:56:03 +0900

    UsageProcessBuilderRedirect

---
+++ java-7-new-features-cook-book/src/main/java/ch09/UsageProcessBuilderRedirect.java
@@ -0,0 +1,27 @@
+package ch09;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UsageProcessBuilderRedirect {
+    public static void main(String[] args) throws IOException {
+        File commands = new File("java-7-new-features-cook-book/src/main/resources/ProcessCommands.txt");
+        File output = new File("/tmp/ProcessLog.txt");
+        File errors = new File("/tmp/ErrorLog.txt");
+
+        ProcessBuilder pb = new ProcessBuilder("/bin/sh");
+        System.out.println(pb.redirectInput());
+        System.out.println(pb.redirectOutput());
+        System.out.println(pb.redirectError());
+
+        pb.redirectInput(commands);
+        pb.redirectOutput(output);
+        pb.redirectError(errors);
+
+        System.out.println(pb.redirectInput());
+        System.out.println(pb.redirectOutput());
+        System.out.println(pb.redirectError());
+
+        pb.start();
+    }
+}
