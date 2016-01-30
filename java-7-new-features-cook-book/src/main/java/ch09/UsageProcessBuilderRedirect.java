83a943818321dd89fc128504c7547d353e66867b
KwonNam Son <kwon37xi@gmail.com>
2016-01-30 23:56:34 +0900

    UsageProcessBuilderRedirect

--- java-7-new-features-cook-book/src/main/java/ch09/UsageProcessBuilderRedirect.java
+++ java-7-new-features-cook-book/src/main/java/ch09/UsageProcessBuilderRedirect.java
@@ -3,6 +3,9 @@ package ch09;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * ProcessBuilder로 프로세스 생성시 input/output/error 를 명시적으로 리다이렉트 시킬 수 있다.
+ */
 public class UsageProcessBuilderRedirect {
     public static void main(String[] args) throws IOException {
         File commands = new File("java-7-new-features-cook-book/src/main/resources/ProcessCommands.txt");
