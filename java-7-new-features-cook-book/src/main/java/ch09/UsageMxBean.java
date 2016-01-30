3d874f8f9ef93c2ef689b9ad6c10f3ed903da81d
KwonNam Son <kwon37xi@gmail.com>
2016-01-30 23:47:05 +0900

    UsageMxBean add OS MXBean

--- java-7-new-features-cook-book/src/main/java/ch09/UsageMxBean.java
+++ java-7-new-features-cook-book/src/main/java/ch09/UsageMxBean.java
@@ -2,10 +2,12 @@ package ch09;
 
 
 import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
 import java.lang.management.RuntimeMXBean;
 
 public class UsageMxBean {
     public static void main(String[] args) {
+        // java.lang.management.PlatformManagedObject 를 상속하는 MXBean을 얻는다.
         RuntimeMXBean mxBean = ManagementFactory.getPlatformMXBean(RuntimeMXBean.class);
 
         System.out.println("JVM Name: " + mxBean.getName());
@@ -14,5 +16,10 @@ public class UsageMxBean {
         System.out.println("JVM Implementation Name: " + mxBean.getVmName());
         System.out.println("JVM Implementation Vendor: " + mxBean.getVmVendor());
         System.out.println("JVM Implementation Version: " + mxBean.getVmVersion());
+
+        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+        System.out.println("JVM OS Name: " + operatingSystemMXBean.getName());
+        System.out.println("JVM OS Version: " + operatingSystemMXBean.getVersion());
+        System.out.println("JVM OS Architecture: " + operatingSystemMXBean.getArch());
     }
 }
